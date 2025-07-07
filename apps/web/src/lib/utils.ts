11dcf1c452385b6d6f9004f2ba9669a33c2fe091
Jie <linsijia628@gmail.com>
2025-07-07 14:18:18 +0800

    wip

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export const getUrl = (path: string) => {
+	if (path.startsWith("/")) {
+		return import.meta.env.PAYLOAD_SITE_URL + path;
+	}
+	if (path.startsWith("http")) {
+		return path;
+	}
+	return `${import.meta.env.PAYLOAD_SITE_URL}/${path}`;
+};
+
+export function kebabToPascal(kebab: string): string {
+	return kebab
+		.split("-")
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+		.join("");
 }
